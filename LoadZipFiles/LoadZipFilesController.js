c90e7df53424b280ae2b9e687a34bed1c8a6985d
Yurii Shyshkin <115140037+YuriiShyshkin@users.noreply.github.com>
2023-06-16 18:56:14 +0300

    Add files via upload

--- LoadZipFiles/LoadZipFilesController.js
+++ LoadZipFiles/LoadZipFilesController.js
@@ -1,38 +1,22 @@
 ({
-	afterScriptsLoaded : function(component, event, helper) {
-		console.log('ScriptLoaded');
-	},
+    afterScriptsLoaded : function(component, event, helper) {
+        console.log('ScriptLoaded');
+    },
     
-    validateFiles : function(component, event, helper) {
+    runJs : function(component, event, helper) {
         let startTime = Date.now();
         if (component.find('toggle').get('v.checked')) {
             
-            var chosenFiles = component.find("files").getElement().files;
-            Promise.all([].map.call(chosenFiles, function (file) {
-                 return new Promise(function (resolve, reject) {  
-                    var reader = new FileReader();         
-                     reader.onload = function () {
-                        resolve({file: new Uint8Array(reader.result)});
-                    };
-                    reader.readAsArrayBuffer(file);
-                 });
-            })).then(function (convertedFiles) {
-                helper.validateFiles(component, helper, convertedFiles, startTime);
-            });
+            helper.runLib(component, helper, startTime); 
             
         } else {
             
-           helper.stringToArrayLike(component); 
+           helper.arrayToString(component); 
             
         }
     },
     
     toogleInput : function(component, event, helper) {
-        var picklist = component.find("select");
-        var customCodeBtn = component.find("customCodeBtn");
-        var chooser = component.find("chooser");
-        $A.util.toggleClass(picklist, "toggle");
-        $A.util.toggleClass(customCodeBtn, "toggle");
-        $A.util.toggleClass(chooser, "toggle");
+        helper.toggleInput(component);
     },
 })
--- LoadZipFiles/LoadZipFilesHelper.js
+++ LoadZipFiles/LoadZipFilesHelper.js
@@ -1,19 +1,31 @@
 ({
-	validateFiles : function(component, helper, convertedFiles, startTime) {
+	runLib : function(component, helper, startTime) {
+        
         var chosenFiles = component.find("files").getElement().files;
-        var chosenFilesLength = chosenFiles.length;
-        var promises = [];
-        for (let i = 0; i < chosenFilesLength; ++i) {
-			promises.push(this.extractAttachments(convertedFiles[i].file, this, component));         
-        }
-        console.log('Before Promise All');
-        Promise.all(promises).then(
-            function(extractedZipInfos){
-				let end = Date.now();
-				let elapsed = end - startTime;
-                console.log('Time of Lib code opperation:', elapsed/1000 + ' sec');
+        Promise.all([].map.call(chosenFiles, function (file) {
+            return new Promise(function (resolve, reject) {  
+                var reader = new FileReader();         
+                reader.onload = function () {
+                    resolve({file: new Uint8Array(reader.result)});
+                };
+                reader.readAsArrayBuffer(file);
+            });
+        })).then(function (convertedFiles) {
+            var chosenFiles = component.find("files").getElement().files;
+            var chosenFilesLength = chosenFiles.length;
+            var promises = [];
+            for (let i = 0; i < chosenFilesLength; ++i) {
+                promises.push(helper.extractAttachments(convertedFiles[i].file, this, component));         
             }
-        );
+            console.log('Before Promise All');
+            Promise.all(promises).then(
+                function(extractedZipInfos){
+                    let end = Date.now();
+                    let elapsed = end - startTime;
+                    console.log('Time of Lib code opperation:', elapsed/1000 + ' sec');
+                }
+            );
+        });     
     },
     
      extractAttachments : function(file, fileName, helper, component) {
@@ -25,23 +37,33 @@
         }));
     },
     
-    stringToArrayLike : function (component) {
+    arrayToString : function (component) {
         let startTime = Date.now();
         let str = '';
-		let array = [];
+        let array = [];
         let maxIter = component.find('select').get('v.value');
         for (var i = 0; i < maxIter; i++) {
             array.push(i);
             str += '' + i;
         }
+        
         for (var i = 0; i < str.length; ++i) {
-            array[i] = str.charCodeAt(i) & 0xFF;
+            array[i] = i;
         }
+        
         let arrayStr = JSON.stringify(array);
         
         let end = Date.now();
         let elapsed = end - startTime;
-        console.log('Time of Custom code opperation:', elapsed/1000 + ' sec');
-        return array;
-	},
+        console.log('Time of Custom code opperation:', elapsed / 1000 + ' sec');
+    },
+    
+    toggleInput : function(component) {
+        var picklist = component.find("select");
+        var customCodeBtn = component.find("customCodeBtn");
+        var chooser = component.find("chooser");
+        $A.util.toggleClass(picklist, "toggle");
+        $A.util.toggleClass(customCodeBtn, "toggle");
+        $A.util.toggleClass(chooser, "toggle");
+    }
 })
